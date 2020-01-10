5b1d63fee4de0ee95977f125142d7f93a3d63314
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-10 07:34:39 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,34 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  def highest_rating
+  
+  def self.highest_rating
     Show.maximum(:rating)
+  end
+  
+  def self.most_popular_show
+    show = Show.where("#{highest_rating}").order(rating: :desc)
+    show[0]
+  end
+  
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+  
+  def self.least_popular_show
+    show = Show.where("#{lowest_rating}").order(rating: :asc)
+    show[0]
+  end
+  
+  def self.ratings_sum
+    Show.sum("rating")
+  end
+  
+  def self.popular_shows
+    Show.where("rating > 5")
+  end
+  
+  def self.shows_by_alphabetical_order
+    Show.order(:name)
   end
 end
