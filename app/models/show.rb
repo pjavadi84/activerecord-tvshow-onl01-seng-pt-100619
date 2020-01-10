f4fb044eef1a503b5a6ac12b4260afcf3e3c7e1d
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-10 07:04:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,7 @@ class Show < ActiveRecord::Base
   
   def self.most_popular_show
     # Show.where("#{highest_rating}").order(:name)
-    show = Show.where("#{highest_rating}").order(rating: :desc)
+    show = Show.where("#{highest_rating}").order(rating: :desc)[0]
       binding.pry
   end
 end
