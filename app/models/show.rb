79df4558f8141e2795972fa40108a0bd79466620
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-10 06:31:28 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,11 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
+  end
+  
+  def self.most_popular_show
+    Show.find_by(:name).where(highest_rating)
   end
 end
